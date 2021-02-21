b70c058b0b415af3765eb660d88881217720b6d4
Tom MORGAN <tommorgan@Toms-MacBook-Pro.local>
2021-02-20 19:52:37 -0800

    updated pool and app ids

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_ccE4gyzob', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6q46umi0cv0ac9somt30b1jbv9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
